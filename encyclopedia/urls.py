bbdcc21575f7e08ee55d31273129d3618d9edd8f
Jamie Doyle <jamedoyle@Jamies-MacBook-Air.local>
2024-09-02 00:22:14 +0100

    3 new features

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:entry_name>",views.visit, name="visit"),
+    path("search/", views.search, name="search"),  # Add this line for search functionality
+    path("new_entry/", views.create_new_entry, name="new_entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import Http404, HttpResponse
 
 from . import util
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
     })
 
+def visit(request, entry_name):
+
+    entry_content = util.get_entry(entry_name)
+
+    if entry_content is None:
+        raise Http404("Page not found!")
+
+    return render(request, "encyclopedia/entry.html", {
+        "entryName": entry_name,
+        "entry": util.get_entry(entry_name)
+    })
+
+def search(request):
+    query = request.GET.get('q', '')  # Get the search query from GET request
+    if util.get_entry(query) is not None:
+        return redirect('visit', entry_name=query)  # Redirect to the entry page if exists
+    else:
+        return HttpResponse("The requested page was not found.")  # You can handle a search failure differently
+
+def create_new_entry(request):
+    return render(request, "encyclopedia/new_entry.html")
